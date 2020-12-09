d434cd96cf460b8acab48a84d23597c550be5782
Przemyslaw Karczmarczyk <przemyslaw.karczmarczyk@cern.ch>
2020-12-09 13:28:42 +0000

    feat: create and plot first fit

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 from scipy.stats import linregress
 
 def draw_plot():
@@ -10,7 +11,9 @@ def draw_plot():
     plt.scatter(x=df['Year'], y=df['CSIRO Adjusted Sea Level'])
 
     # Create first line of best fit
-
+    slope, intercept = linregress(x=df['Year'], y=df['CSIRO Adjusted Sea Level'])[0:2]
+    ext_years = np.arange(df['Year'].iloc[0], 2050)
+    plt.plot(ext_years, intercept + slope*ext_years, 'r')
 
     # Create second line of best fit
 
