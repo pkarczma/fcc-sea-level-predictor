4d5b369172c1817350e93b547318d9e0ba55c144
Przemyslaw Karczmarczyk <przemyslaw.karczmarczyk@cern.ch>
2020-12-10 16:35:49 +0000

    fix: auto format code

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import linregress
 
+
 def draw_plot():
     # Read data from file
     df = pd.read_csv('epa-sea-level.csv')
@@ -11,19 +12,21 @@ def draw_plot():
     plt.scatter(x=df['Year'], y=df['CSIRO Adjusted Sea Level'])
 
     # Create first line of best fit
-    slope, intercept = linregress(x=df['Year'], y=df['CSIRO Adjusted Sea Level'])[0:2]
+    slope, intercept = linregress(
+        x=df['Year'], y=df['CSIRO Adjusted Sea Level'])[0:2]
     ext_years = np.arange(df['Year'].iloc[0], 2050)
-    plt.plot(ext_years, intercept + slope*ext_years, 'r')
+    plt.plot(ext_years, intercept + slope * ext_years, 'r')
 
     # Create second line of best fit
     mask = df['Year'] > 2000
-    slope, intercept = linregress(x=df.loc[mask, 'Year'], y=df.loc[mask, 'CSIRO Adjusted Sea Level'])[0:2]
+    slope, intercept = linregress(
+        x=df.loc[mask, 'Year'],
+        y=df.loc[mask, 'CSIRO Adjusted Sea Level'])[0:2]
     ext_years = np.arange(2000, 2050)
-    plt.plot(ext_years, intercept + slope*ext_years, 'b')
+    plt.plot(ext_years, intercept + slope * ext_years, 'b')
 
     # Add labels and title
 
-    
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
